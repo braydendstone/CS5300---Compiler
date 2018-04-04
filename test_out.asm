b2da14b957c280e159d4942e0c0eb08f16817113
braydendstone <braydendstone@gmail.com>
2018-04-03 23:36:18 -0600

    test 2

--- test_out.asm
+++ test_out.asm
@@ -30,7 +30,30 @@ add $s6, $s4, $s5
 move $s4, $gp
 addi $s4, $s4, 0
 sw $s6, 0($s4)
+WHILE1: 
+move $s6, $gp
+addi $s6, $s6, 0
+lw $s4, 0($s6)
+lw $s5, 0($gp)
+li $s4, 23
+seq $s6, $s5, $s4
+beq $s6, $0, WHILE1_END
+la $a0, STR0
+li $v0, 4
+syscall
+move $s5, $gp
+addi $s5, $s5, 0
+lw $s6, 0($s5)
+lw $s4, 0($gp)
+li $s6, 1
+add $s5, $s4, $s6
+move $s4, $gp
+addi $s4, $s4, 0
+sw $s5, 0($s4)
+j WHILE1
+WHILE1_END:
 j WHILE0
 WHILE0_END:
 .data
 .asciiz
+STR0: "innter while"
