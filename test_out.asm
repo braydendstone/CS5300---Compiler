02a5fd0d9cf8ff413a6722f8f76d8b8064b98027
braydendstone <braydendstone@gmail.com>
2018-04-03 23:30:55 -0600

    test

---
+++ test_out.asm
@@ -0,0 +1,36 @@
+.globl main
+.code
+main:
+j start
+start: 
+li $s7, 20
+move $s6, $gp
+addi $s6, $s6, 0
+sw $s7, 0($s6)
+WHILE0: 
+move $s7, $gp
+addi $s7, $s7, 0
+lw $s6, 0($s7)
+lw $s5, 0($gp)
+li $s6, 25
+slt $s7, $s5, $s6
+beq $s7, $0, WHILE0_END
+move $s5, $gp
+addi $s5, $s5, 0
+lw $s7, 0($s5)
+move $a0, $s7
+li $v0, 1
+syscall
+move $s6, $gp
+addi $s6, $s6, 0
+lw $s5, 0($s6)
+lw $s4, 0($gp)
+li $s5, 1
+add $s6, $s4, $s5
+move $s4, $gp
+addi $s4, $s4, 0
+sw $s6, 0($s4)
+j WHILE0
+WHILE0_END:
+.data
+.asciiz
