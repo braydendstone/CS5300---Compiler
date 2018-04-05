f8bb5862086c0526c0a2012ebe5ce26360025408
braydendstone <braydendstone@gmail.com>
2018-04-05 00:08:01 -0600

    example ifelse

--- test_out.asm
+++ test_out.asm
@@ -13,11 +13,18 @@ lw $s6, 0($s7)
 lw $s5, 0($gp)
 li $s6, 5
 slt $s7, $s5, $s6
-beq $s7, $0, IF0_END
+beq $s7, $0, IF0EL0
 la $a0, STR0
 li $v0, 4
 syscall
+j IF0_END
+IF0EL0:
+la $a0, STR1
+li $v0, 4
+syscall
+j IF0_END
 IF0_END:
 .data
 .asciiz
 STR0: "x less than 5"
+STR1: "x is greater than or equal to 5"
